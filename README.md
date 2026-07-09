45d2696c20f293cc46df8300d57af21c67240e11
Fares <fares.oyion123@gmail.com>
2026-07-09 18:03:13 +0300

    Add deploy workflow and update astro config

---
+++ README.md
@@ -0,0 +1,2 @@
+# My-Portfolio
+A web Application For Showing My Projects
--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import react from '@astrojs/react';
 
 export default defineConfig({
     integrations: [tailwind(), react()],
-    site: 'https://yourusername.github.io', // استبدله برابط موقعك
-    base: '/my-portfolio', // اسم المستودع (إذا كان الموقع في مستودع باسم portfolio مثلاً)
+    site: 'https://Fares-knight-2005.github.io', // استبدله برابط موقعك
+    base: '/My-Portfolio', // اسم المستودع (إذا كان الموقع في مستودع باسم portfolio مثلاً)
 });
