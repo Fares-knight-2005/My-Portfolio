6df1a37b159a5cbafede34dc905f8842b2aced09
Fares <fares.oyion123@gmail.com>
2026-07-09 19:18:23 +0300

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import react from '@astrojs/react';
 
 export default defineConfig({
     integrations: [tailwind(), react()],
-    site: 'https://Fares-knight-2005.github.io', // استبدله برابط موقعك
-    base: '/My-Portfolio', // اسم المستودع (إذا كان الموقع في مستودع باسم portfolio مثلاً)
+    site: 'https://Fares-knight-2005.github.io',
+    base: '/My-Portfolio',
 });
